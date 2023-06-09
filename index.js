0152e244e1e429267fb1d1b3e63d9cbfdae5b50a
Brian Kibet <kibetsbrian@gmail.com>
2023-06-09 14:49:45 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string){
+    return string.toUpperCase()
+}
+shout("hello")
+
+
+// whisper function
+
+function whisper(string){
+    return string.toLowerCase()
+}
+whisper("I am in lower case")
+console.log()
+
+// logshout function
+function logShout (string){
+    console.log(string.toUpperCase)
+}
+logShout("hello!")
+
+// logwhisper function
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+logWhisper("HELLO!")
+
+sayHiToHeadphonedRoommate
+
+function sayHiToHeadphonedRoommate(string){
+    if (string == string.toUpperCase()){
+        return "YES INDEED!"
+    }else if (string == string.toLowerCase()){
+        return "I can\'t hear you!"
+    }else(string == "Let\'s have dinner together!")
+        return "I would love to!"
+
+    return string
+}
+sayHiToHeadphonedRoommate('hello'.toUpperCase)
+sayHiToHeadphonedRoommate('hello'.toLowerCase)
+sayHiToHeadphonedRoommate( "Let\'s have dinner together!")
+
+
